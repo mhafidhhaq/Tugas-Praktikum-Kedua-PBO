fe7e4b2d6b7eff13ba271dc85afd31e4bf857717
mhafidhhaq <mhafidhdliyaulh11@gmail.com>
2019-12-14 14:32:25 +0700

    coba

---
+++ Back End/buat_laporan.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Buat Laporan</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+	<div class="pembungkus">
+		<header>SIMPLE LAPOR!</header>
+
+		<div class="konten">
+			<p>Buat Laporan/Komentar</p>
+			<hr><br>
+			<input type="text" name="nama" id="nama" value="Laporan/Komentar"><br>
+			<select>
+				<option value="">Pilih Aspek Pelaporan/Komentar</option>
+				<option value="Ruang Kelas">Ruang Kelas</option>
+				<option value="Dosen">Dosen</option>
+				<option value="Gedung">Gedung</option>
+				<option value="Parkiran">Parkiran</option>
+				<option value="Jalanan">Jalanan</option>
+			</select> <br><br>
+			<input type="file" name=""> <br><br>
+			<button type="submit">Buat LAPOR!</button>
+			<br><br><br>
+			<hr>
+		</div>
+	</div>
+</body>
+
+</html>
---
+++ Back End/halaman_utama.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Halaman Utama</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+	<div class="pembungkus">
+		<header>
+			SIMPLE LAPOR!
+			<div class="search-box">
+				<form action="" method="POST">
+					<input name="search" required="yes" type="text">
+					<button type="submit"><img src="img/icon-search.png">Cari</button>
+				</form>
+			</div>
+			<p>Buat Laporan/Komentar <a href="buat_laporan.html"><img src="img/insert.png"></a></p>
+		</header>
+
+		<div class="konten">
+			<p>Laporan/Komentar Terakhir</p>
+			<hr>
+			<p>Lorem ipsum dolor sit amet, consectur adipiscing elit. Nunc maximus, nulla ut commodo sagittis, sapien
+				dui mattis dui, non pulvinar lorem felis nec erat. Lorem ipsum dolor sit amet, consectur adipiscing
+				elit. Nunc maximus, nulla ut commodo sagittis, sapien dui mattis dui, non pulvinar lorem felis nec erat.
+				Lorem ipsum dolor sit amet, consectur adipiscing elit. Nunc maximus, nulla ut commodo sagittis, sapien
+				dui mattis dui, non pulvinar lorem felis nec erat</p>
+			<div class="lampiran">
+				Lampiran: Gambar.jpg
+			</div>
+
+			<div class="selengkapnya">
+				Waktu: 20-11-2019 20:00
+				<a href="">Lihat Selengkapnya ></a>
+			</div>
+			<hr>
+
+			<p>Lorem ipsum dolor sit amet, consectur adipiscing elit. Nunc maximus, nulla ut commodo sagittis, sapien
+				dui mattis dui, non pulvinar lorem felis nec erat. Lorem ipsum dolor sit amet, consectur adipiscing
+				elit. Nunc maximus, nulla ut commodo sagittis, sapien dui mattis dui, non pulvinar lorem felis nec erat.
+				Lorem ipsum dolor sit amet, consectur adipiscing elit. Nunc maximus, nulla ut commodo sagittis, sapien
+				dui mattis dui, non pulvinar lorem felis nec erat</p>
+			<div class="lampiran">
+				Lampiran: Gambar.jpg
+			</div>
+
+			<div class="selengkapnya">
+				Waktu: 20-11-2019 20:00
+				<a href="">Lihat Selengkapnya ></a>
+			</div>
+			<hr>
+
+			<p>Lorem ipsum dolor sit amet, consectur adipiscing elit. Nunc maximus, nulla ut commodo sagittis, sapien
+				dui mattis dui, non pulvinar lorem felis nec erat. Lorem ipsum dolor sit amet, consectur adipiscing
+				elit. Nunc maximus, nulla ut commodo sagittis, sapien dui mattis dui, non pulvinar lorem felis nec erat.
+				Lorem ipsum dolor sit amet, consectur adipiscing elit. Nunc maximus, nulla ut commodo sagittis, sapien
+				dui mattis dui, non pulvinar lorem felis nec erat</p>
+			<div class="lampiran">
+				Lampiran: Gambar.jpg
+			</div>
+
+			<div class="selengkapnya">
+				Waktu: 20-11-2019 20:00
+				<a href="">Lihat Selengkapnya ></a>
+			</div>
+			<hr>
+		</div>
+
+		<footer>
+			<br><img src="img/menu-option.png"><br>
+			<img src="img/menu-option.png">
+		</footer>
+	</div>
+	<!-- <div class="kotak-pencarian">
+		<input type="search" name="cari">
+	</div>
+	<button type="submit"></button> -->
+</body>
+
+</html>
